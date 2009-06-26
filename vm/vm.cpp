854c5ef47c5b736838457ccb6fd73e8a362e5fba
ksugltrongreen <ksugltrongreen@d7243bb4-61b4-11de-a47d-7741ea9f0571>
2009-06-26 20:46:31 +0000

    Скелетная версия виртуальной машины.

---
+++ vm/vm.cpp
@@ -0,0 +1,34 @@
+typedef int Command;
+typedef double MemoryCell;
+MemoryCell inputPorts[16384];
+MemoryCell outputPorts[16384];
+MemoryCell data[16384];
+Command instructions[16384];
+long instructionCounter;
+void load(char* filename)
+{
+}
+void execCommand(Command command)
+{
+}
+void printOutputPorts()
+{
+}
+void getInputPorts()
+{
+
+}
+int main(int argc, char *argv[])
+{
+  load(argv[1]);
+  instructionCounter=0;
+  while (true)
+  {
+	 getInputPorts();
+	 execCommand(instructions[instructionCounter]);
+	 printOutputPorts();
+	 if (outputPorts[0]<>0.0) return 0;
+	 instructionCounter++; instructionCounter &= (1 << 15 - 1);
+  }
+  return 123;
+}
