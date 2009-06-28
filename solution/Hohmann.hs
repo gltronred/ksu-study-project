d10b05c5398218cd49c59960c93c2ac248c17a43
buggger <buggger@d7243bb4-61b4-11de-a47d-7741ea9f0571>
2009-06-28 20:12:31 +0000

    Realized Hohmann without phi

--- solution/Hohmann.hs
+++ solution/Hohmann.hs
@@ -3,13 +3,17 @@ module Hohmann
 
 data Burst = Burst Int Double Double
 
-moveHohmann :: Int -> Double -> Double -> [Burst]
-moveHohmann t0 r1 r2 = 
-    [Burst t1 power1*cos(phi) power1*sin(phi), Burst t2 power2*cos(phi) -power2*sin(phi)]
+moveHohmann :: Int -> Double -> Double -> Double -> Double -> [Burst]
+moveHohmann t0 x y r2 phi = 
+    [Burst t1 (power1*cos(phi)) (power1*sin(phi)), Burst t2 (power2*cos(phi)) (-power2*sin(phi))]
     where
+      gravitationalConstant = 6.67428e-11
+      massOfEarth = 6.0e24
+      mu = gravitationalConstant*massOfEarth
       t1 = t0
-      t2 = t1 + tH
-      tH = t0
-      power1 = 0.0
-      power2 = 0.0
+      r1 = sqrt(x*x + y*y)
+      t2 = floor $ fromIntegral t1 + tH
+      tH = pi * sqrt( (r1+r2)^3/8/mu )
+      power1 = sqrt(mu/r1) * (sqrt(2*r2/(r1+r2)) - 1)
+      power2 = sqrt(mu/r2) * (1 - sqrt(2*r1/(r1+r2)))
       phi = 0.0
