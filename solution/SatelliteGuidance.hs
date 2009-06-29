497b350616a9e481b454ed7923da2a568ca9c297
ksugltrongreen <ksugltrongreen@d7243bb4-61b4-11de-a47d-7741ea9f0571>
2009-06-29 08:50:27 +0000

    Ввел в программу ввод-вывод. Функции runIOProgram на вход подается значение типа Program, а на выходе получается бесконечный поток вывода (в формате, который понимается VM), То есть теперь, чтобы получить решение, нужен просто список пар вида (время, [(номер порта, значение)]).

--- solution/SatelliteGuidance.hs
+++ solution/SatelliteGuidance.hs
@@ -5,6 +5,7 @@ import Serializer
 import Data.Word
 import Control.Monad
 data Program = Program [(Word32, [(Word32, Double)])]
+data CompactifiedInteger = Number Integer | InfinitePoint deriving Show
 
 getPVM (addr, value) = PVMapping addr value
 getPVMs = map getPVM
@@ -15,9 +16,22 @@ getSolution (Program list) = Solution ourHeader ((>>= getFrame) list)
 getSolutionTerminated (Program list) terminationMoment = Solution ourHeader (((>>= getFrame) list) ++ [Frame terminationMoment 0 []]) 
 filterUnique list1 list2 = map snd $ filter (\ (a,b) -> a /= b) $ zip list1 list2 
 filterSimFrame (i, list1) (j, list2) = (j, filterUnique list1 list2)
-filterProgramList [] = []
-filterProgramList l = head l:filterProgramList' l
-filterProgramList' [] = []
-filterProgramList' (x:xs) = if (null xs) then []
-                                     else (filterSimFrame x (head xs)):filterProgramList' xs
-filterProgram (Program list) = Program (filterProgramList list)
+finDiff op [] = []
+finDiff op l = head l:finDiff' op l
+finDiff' op [] = []
+finDiff' op (x:xs) = if (null xs) then []
+                                  else (op x (head xs)):finDiff' op xs
+filterProgram (Program list) = Program (finDiff  filterSimFrame list)
+
+subtractCompactified (Number a) (Number b) = Number (a - b)
+subtractCompactified _ _ = InfinitePoint
+
+compactifyProgram (Program list) = map (\(a,l)->(Number (fromIntegral a), map snd l)) list
+augmentWithInfinity l = (tail l) ++ [(InfinitePoint, [])]
+subtractFrames (a, l1) (b, l2) = (subtractCompactified a b, l2)
+getFinDiff l = zipWith subtractFrames (augmentWithInfinity l) l
+printPorts = putStrLn . unwords . map show
+createIOStream (Number a, ports) = replicateM_ (fromIntegral a) $ printPorts ports
+createIOStream (InfinitePoint, ports) = forever $ printPorts ports
+runIOStream list = sequence_ [createIOStream l| l <- list]
+runIOProgram = runIOStream . getFinDiff . compactifyProgram
