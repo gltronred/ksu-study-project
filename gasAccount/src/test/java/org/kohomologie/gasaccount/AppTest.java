8a8e074054789f9aa4035396d007f95cc0e147d8
ksugltrongreen <ksugltrongreen@d7243bb4-61b4-11de-a47d-7741ea9f0571>
2009-09-25 08:04:32 +0000

    Exported test java pom.xml

---
+++ gasAccount/src/test/java/org/kohomologie/gasaccount/AppTest.java
@@ -0,0 +1,20 @@
+package org.kohomologie.gasaccount;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void ololo()
+    {
+       assert 5 == 5;
+    }
+}
